3c05f2bc9a0522691ce1593a6ec146edea6eb157
olamide <myyde8170@gmail.com>
2022-04-05 22:16:14 +0100

    creating an authentication request and response

---
+++ src/main/java/com/example/jwtstart/models/AuthRequest.java
@@ -0,0 +1,15 @@
+package com.example.jwtstart.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthRequest {
+
+    private String username;
+    private String password;
+
+}
