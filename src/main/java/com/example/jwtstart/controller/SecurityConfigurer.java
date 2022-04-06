1302019534cddf2bb5105d2a515cafcc206b275a
olamide <myyde8170@gmail.com>
2022-04-06 19:15:12 +0100

    added configure method that lets users to access the authenticated api

--- src/main/java/com/example/jwtstart/controller/SecurityConfigurer.java
+++ src/main/java/com/example/jwtstart/controller/SecurityConfigurer.java
@@ -4,6 +4,7 @@ import com.example.jwtstart.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -17,6 +18,13 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/authenticate").permitAll().
+                anyRequest().authenticated();
     }
 
     @Bean
